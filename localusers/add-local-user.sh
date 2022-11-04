96bc858e8dee4c75ecac60410989229100e8b339
Ray Sylverne <ray@2m1mrealestate.com>
2022-11-04 22:59:32 +0000

    finishd local user script

--- localusers/add-local-user.sh
+++ localusers/add-local-user.sh
@@ -19,21 +19,37 @@ read -p "Enter the password to use for the account: " PASSWORD
 
 # Create the user with the password.
 useradd -c "${COMMENT}" -m ${USER_NAME}
-echo {PASSWORD} | passwd --stdin ${USER_NAME}
 
 # Force password change on first login.
 passwd -e ${USER_NAME}
 
 # Check to see if the useradd command succeeded.
-
+# We don't want to tell the user an account has been created when it hasn't 
+if [[ "${?}" -ne 0 ]]
+then 
+      echo "The user account was not created"
+      exit 1
+fi
 
 # Set the password.
 
+echo {PASSWORD} | passwd --stdin ${USER_NAME}
 
 # Check to see if the passwd command succeeded.
-
-
+if [[ "${?}" -ne 0 ]]
+then
+      echo "The password could not be sent see Sys Admin"
+      exit 1
+fi
 # Force password change on first login.
-
+passwd -e ${USER_NAME}
 
 # Display the username, password, and the host where the user was created.
+echo
+echo "The user account was succefully created"
+echo "Securely provide the user with their login credentials below"
+echo
+echo "username: ${USER_NAME}"
+echo "passwd:  ${PASSWORD}"
+echo
+exit 0
